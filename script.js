7fb1eb05ced90226203cde6ab53581e6d24ecc43
mdshehabislam <shihabkhanahab@gmail.com>
2025-08-29 16:19:03 +0530

    WEB is done

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 // Love Count
 let LoveCount = 0;
 const LoveCountValue = document.getElementById("LoveCount");
-document.querySelectorAll(".LoveCnt").forEach((btn) => {
-  btn.addEventListener("click", () => {
+
+const loveBtn = document.getElementsByClassName("LoveCnt");
+for (let i = 0; i < loveBtn.length; i++) {
+  loveBtn[i].addEventListener("click", function () {
     LoveCount++;
     LoveCountValue.textContent = LoveCount;
   });
-});
+}
 
 // Copy number and count
 let copyCount = 0;
-const copyCounter = document.getElementById("copyCount");
-const copyCountValue =
-  document.querySelector("header .copy-count") ||
-  document.querySelector("span#copyCount");
-document.querySelectorAll(".copy-btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const number = btn.dataset.number;
-    navigator.clipboard.writeText(number);
+const copyCountValue = document.getElementById("copyCount");
+const copyBtn = document.getElementsByClassName("copy-btn");
+
+for (let i = 0; i < copyBtn.length; i++) {
+  copyBtn[i].addEventListener("click", function () {
+    const number = copyBtn[i].dataset.number;
+
     alert("Number was copied: " + number);
     copyCount++;
     copyCountValue.textContent = copyCount;
+
+    navigator.clipboard.writeText(number);
   });
-});
+}
 
 // calling
 let coinCount = 100;
-const coinCounter = document.getElementById("coinCount");
-const coinCountevalue =
-  document.querySelector("header .coin-count") ||
-  document.querySelector("span#coinCount");
+const coinCountValue = document.getElementById("coinCount");
+const callBtn = document.getElementsByClassName("call-btn");
 
-document.querySelectorAll(".call-btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const serviceName = btn.dataset.service;
-    const serviceNumber = btn.dataset.number;
+for (let i = 0; i < callBtn.length; i++) {
+  callBtn[i].addEventListener("click", function () {
+    const service_name = callBtn[i].dataset.service;
+    const service_number = callBtn[i].dataset.number;
 
     if (coinCount < 20) {
       alert("You don't have enough coin!! you need 20 coin make a call");
@@ -43,23 +44,31 @@ document.querySelectorAll(".call-btn").forEach((btn) => {
 
     // reduce coin
     coinCount -= 20;
-    coinCountevalue.textContent = coinCount;
+    coinCountValue.textContent = coinCount;
 
-    alert(`Calling ${serviceName} (${serviceNumber})`);
+    alert(`Calling ${service_name} (${service_number})`);
 
     //Instant Time
     const now = new Date();
     const timeString = now.toLocaleTimeString();
 
     const span = document.createElement("span");
-    span.textContent = `${serviceName} - ${serviceNumber} (${timeString})`;
+    span.innerHTML = `
+  <div class=" ml-4 mr-4 flex my-6 justify-between">
+    <div>
+      <h1 class="inter font-bold">${service_name}<h1>
+      <p class="hind-madurai text-[#5C5C5C]">${service_number}</p>
+    </div>
+    <div class="hind-madurai font-semibold mt-4">${timeString}</div>
+  </div>
+`;
     callHistoryList.appendChild(span);
   });
-});
+}
 
 // clear calling history
-const clearHistoryBtn = document.getElementById("clearHistory");
+const clearCallHistoryBtn = document.getElementById("clearCallHistory");
 const callHistoryList = document.getElementById("callHistoryList");
-clearHistoryBtn.addEventListener("click", () => {
+clearCallHistoryBtn.addEventListener("click", function () {
   callHistoryList.innerHTML = "";
 });
